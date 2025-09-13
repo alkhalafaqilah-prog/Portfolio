373a05cfcbacbb485ff062ef17b4a65892c078bb
Aqilah Alkhalaf <alkhalafaqilah@gmail.com>
2025-09-13 22:19:40 +0300

    Str Final UPDT

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+//Copied from Stacks Overflows
+function validateForm() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (name === "" || email === "" || message === "") {
+        alert("All fields must be filled out!");
+        return false;
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailRegex.test(email)) {
+        alert("Please enter a valid email address.");
+        return false;
+    }
+    return true;
+}
